9c12dc3db881602a6dcc7910fe7a36df1c410d0a
Joshua Lewis <joshualewis@adminisatorsmbp.home>
2021-02-22 21:22:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Form from './Form.js';
+import Cardlist from './Cardlist.js';
+// const axios = require('axios')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      profiles: []
+    }
+  };
+
+  addNewProfile = (profileData) =>{
+    console.log('APP', profileData)
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }))
+  }
+
+  render(){
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Form onSubmit= {this.addNewProfile}/>
+        <Cardlist profiles={this.state.profiles}/>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
